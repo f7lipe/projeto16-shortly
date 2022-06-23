261d9d455649955333987489955426d6597c8304
isabellacgmsa <isabellacgmsa1@gmail.com>
2022-06-23 11:09:33 -0300

    Preparando para revisão de código

--- src/routes/signup/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { validateSignup } from "../../middleware/user/index.js";
-import { createUser } from "../../controllers/user/index.js";
-
-const signupRouter = Router()
-signupRouter.post('/signup', validateSignup, createUser)
-
-export {signupRouter}
--- src/routes/user/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getUserById, getRank } from "../../controllers/user/index.js";
-
-const userRouter = Router()
-userRouter.get('/users/:id', getUserById)
-userRouter.get('/ranking', getRank)
-
-export{userRouter}
